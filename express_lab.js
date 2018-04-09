e0ebce3a40c77be0be304ceb6d3cefa0961dc16c
Hannah Werman <hannah.werman@gmail.com>
2018-04-09 11:31:15 -0500

    tip calculator express application created

--- express_lab.js
+++ express_lab.js
@@ -15,3 +15,15 @@ app.get('/greeting/:name', (req, res) => {
 	// const name = req.params.index
 	res.send("Greetings, " + req.params.name + "! It's so great to see you!")
 })
+
+// TIP CALCULATOR
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+	const data = {
+		"total": parseInt(req.params.total),
+		"tipPercentage": req.params.tipPercentage / 100,
+	}
+
+	console.log(data['total'] * data['tipPercentage'])
+	res.send("Tip amount: " + (data['total'] * data['tipPercentage']).toString())
+})
