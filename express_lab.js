5dfb4061ffd5daf14710a6abbdb9a6156e9988b7
Hannah Werman <hannah.werman@gmail.com>
2018-04-09 11:43:06 -0500

    magic 8 ball express application created

--- express_lab.js
+++ express_lab.js
@@ -27,3 +27,14 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 	console.log(data['total'] * data['tipPercentage'])
 	res.send("Tip amount: " + (data['total'] * data['tipPercentage']).toString())
 })
+
+// MAGIC 8 BALL 
+
+app.get("/magic/:input", (req, res) => {
+	const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+	const question = req.params.input.toString();
+	let randResponse = responses[Math.floor(Math.random() * responses.length)]
+	console.log(question, randResponse)
+	res.send("Your quesion: " + question + "? \n Magic 8 Ball Response: " + randResponse + ".");
+	// res.send(randResponse);
+})
